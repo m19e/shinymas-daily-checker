3bbac5aba2994c1eedae04d987976c134599b3b3
m19e <nexus.me7290@gmail.com>
2020-06-21 02:47:27 +0900

    Open page by event page process

--- dist/event.js
+++ dist/event.js
@@ -1,6 +1,17 @@
+var updateHour = 2;
+var isUpdate = false;
 chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 2 });
 chrome.alarms.onAlarm.addListener(function (alarm) {
     if (alarm.name == "UPDATE_CHECK") {
-        console.log("Ring!");
+        var hour = new Date().getHours();
+        if (updateHour == hour) {
+            if (!isUpdate) {
+                window.open("https://shinycolors.enza.fun/mission", "_blank");
+                isUpdate = true;
+            }
+            else {
+                isUpdate = false;
+            }
+        }
     }
 });
--- src/event.ts
+++ src/event.ts
@@ -1,7 +1,18 @@
+const updateHour: number = 2;
+let isUpdate: boolean = false;
+
 chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 2 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name == "UPDATE_CHECK") {
-        console.log("Ring!");
+        const hour = new Date().getHours();
+        if (updateHour == hour) {
+            if (!isUpdate) {
+                window.open("https://shinycolors.enza.fun/mission", "_blank");
+                isUpdate = true
+            } else {
+                isUpdate = false
+            }
+        }
     }
 });
