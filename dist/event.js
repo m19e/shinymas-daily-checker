d9a582329ee3a6827d1eabb2341c6d2ef09aa1a5
m19e <nexus.me7290@gmail.com>
2020-06-21 14:39:22 +0900

    Shorten interval for debug

--- dist/event.js
+++ dist/event.js
@@ -1,6 +1,6 @@
 var updateHour = 2;
 var isUpdate = false;
-chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 2 });
+chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 1 });
 chrome.alarms.onAlarm.addListener(function (alarm) {
     if (alarm.name == "UPDATE_CHECK") {
         var hour = new Date().getHours();
--- src/event.ts
+++ src/event.ts
@@ -1,7 +1,7 @@
 const updateHour: number = 2;
 let isUpdate: boolean = false;
 
-chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 2 });
+chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 1 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name == "UPDATE_CHECK") {
@@ -9,9 +9,9 @@ chrome.alarms.onAlarm.addListener((alarm) => {
         if (updateHour == hour) {
             if (!isUpdate) {
                 window.open("https://shinycolors.enza.fun/mission", "_blank");
-                isUpdate = true
+                isUpdate = true;
             } else {
-                isUpdate = false
+                isUpdate = false;
             }
         }
     }
