a28cc0965ca65c931f9528f1519e723b338c983e
m19e <nexus.me7290@gmail.com>
2020-06-22 12:33:58 +0900

    Open mission page per day

--- dist/event.js
+++ dist/event.js
@@ -1,17 +1,48 @@
-var updateHour = 2;
-var isUpdate = false;
-chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 1 });
+var getStorage = function (key) {
+    var value;
+    chrome.storage.local.get(key, function (v) {
+        value = v;
+        console.log("Get " + key + ":" + v[key]);
+    });
+    return value;
+};
+var setStorageUpdated = function (updated) {
+    chrome.storage.local.set({ UPDATED: updated }, function () {
+        console.log("Set UPDATED:" + updated);
+    });
+};
+var init = function () {
+    chrome.alarms.clearAll();
+    chrome.storage.local.clear();
+    setStorageUpdated(false);
+    chrome.alarms.create("UPDATED_CHECK", { delayInMinutes: 1 });
+};
+init();
+var updateHour = 5;
+var updated = getStorage("UPDATED");
 chrome.alarms.onAlarm.addListener(function (alarm) {
-    if (alarm.name == "UPDATE_CHECK") {
-        var hour = new Date().getHours();
-        if (updateHour == hour) {
-            if (!isUpdate) {
-                window.open("https://shinycolors.enza.fun/mission", "_blank");
-                isUpdate = true;
-            }
-            else {
-                isUpdate = false;
-            }
+    // const now = new Date("2006 7 29 5:38 +0900");
+    var now = new Date();
+    if (alarm.name == "UPDATED_CHECK") {
+        if (!updated) {
+            window.open("https://shinycolors.enza.fun/mission", "_blank");
+            setStorageUpdated(true);
+            console.log("check daily mission");
+        }
+        chrome.alarms.clearAll();
+        chrome.alarms.create("RUN_CHECK", { periodInMinutes: 60 });
+        console.log("goto run_check");
+    }
+    if (alarm.name == "RUN_CHECK") {
+        if (now.getHours() == updateHour) {
+            setStorageUpdated(false);
+            console.log("its time:", now.getMinutes());
+            chrome.alarms.clearAll();
+            chrome.alarms.create("UPDATED_CHECK", { periodInMinutes: 1 });
+            console.log("goto updated_check");
+        }
+        else {
+            console.log("not today");
         }
     }
 });
--- src/event.ts
+++ src/event.ts
@@ -1,18 +1,54 @@
-const updateHour: number = 2;
-let isUpdate: boolean = false;
+const getStorage = (key: string): any => {
+    let value: any;
+    chrome.storage.local.get(key, (v) => {
+        value = v;
+        console.log(`Get ${key}:${v[key]}`);
+    });
+    return value;
+};
 
-chrome.alarms.create("UPDATE_CHECK", { delayInMinutes: 1, periodInMinutes: 1 });
+const setStorageUpdated = (updated: boolean) => {
+    chrome.storage.local.set({ UPDATED: updated }, () => {
+        console.log(`Set UPDATED:${updated}`);
+    });
+};
+
+const init = () => {
+    chrome.alarms.clearAll();
+    chrome.storage.local.clear();
+    setStorageUpdated(false);
+    chrome.alarms.create("UPDATED_CHECK", { delayInMinutes: 1 });
+};
+
+init();
+
+const updateHour: number = 5;
+const updated: boolean = getStorage("UPDATED");
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name == "UPDATE_CHECK") {
-        const hour = new Date().getHours();
-        if (updateHour == hour) {
-            if (!isUpdate) {
-                window.open("https://shinycolors.enza.fun/mission", "_blank");
-                isUpdate = true;
-            } else {
-                isUpdate = false;
-            }
+    // const now = new Date("2006 7 29 5:38 +0900");
+    const now = new Date();
+
+    if (alarm.name == "UPDATED_CHECK") {
+        if (!updated) {
+            window.open("https://shinycolors.enza.fun/mission", "_blank");
+            setStorageUpdated(true);
+            console.log("check daily mission");
+        }
+        chrome.alarms.clearAll();
+        chrome.alarms.create("RUN_CHECK", { periodInMinutes: 60 });
+        console.log("goto run_check");
+    }
+
+    if (alarm.name == "RUN_CHECK") {
+        if (now.getHours() == updateHour) {
+            setStorageUpdated(false);
+            console.log("its time:", now.getMinutes());
+            chrome.alarms.clearAll();
+            chrome.alarms.create("UPDATED_CHECK", { periodInMinutes: 1 });
+            console.log("goto updated_check");
+        } else {
+            console.log("not today");
         }
     }
 });
